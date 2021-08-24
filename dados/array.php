20a6a9a649010f8a8a31adadb9a9bce1a80c5e2a
alxmart <alxmartinez@gmail.com>
2021-08-24 18:01:25 -0300

    Criado arquivo array.php para armazenar o array a ser usado e deletando arquivo teste1.txt.

---
+++ dados/array.php
@@ -0,0 +1,14 @@
+<?php
+
+  $valores = [
+    ["id" => 1,
+      "Doce" => "Brigadeiro",
+      "Quantidademinima" => 50,
+      "Valor" => 85.00,
+      "Prazoproducaodias" => 3,
+      "Prazocomentregadias" => 4,
+      "Ingredientes" => "Açucar, leite cond,chocolate belga",
+    ],
+        
+  ];
+?>
