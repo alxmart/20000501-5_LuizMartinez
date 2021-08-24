40f3a0312a86541503bcbe08bd9b915e3b71906c
alxmart <alxmartinez@gmail.com>
2021-08-24 18:28:40 -0300

    Ajustes no img src acertando caminho e  ajustando imagem / banner da empresa.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="css/index.css">
+  <link rel="stylesheet" href="./css/index.css">
   <title>Doces Caseiros ACME</title>
 </head>
 <body>
@@ -13,7 +13,7 @@
     <table border="3" bgcolor="pink" width="100%">
       <tr>
           <td align="center">
-                  <img src="images/luizao-doces-caseiros.png" alt="Banner da Empresa" height="250px">
+                  <img src="./images/luizao-doces-caseiros.png" alt="Banner da Empresa" height="350px">
           </td>
       </tr>
     </table>
